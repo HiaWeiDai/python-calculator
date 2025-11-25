ab7d874af7830e1345583dd793d019b64438a891
HiaWeiDai <s10269256@connect.np.edu.sg>
2025-11-25 10:27:00 +0800

    Break test on purpose

--- test_calculator.py
+++ test_calculator.py
@@ -6,7 +6,7 @@ from calculator import add, subtract, multiply, divide
 def test_add():
     """Test addition function."""
     assert add(2, 3) == 5
-    assert add(-1, 1) == 0
+    assert add(-1, 1) == 6
     assert add(0, 0) == 0
 
 def test_subtract():
